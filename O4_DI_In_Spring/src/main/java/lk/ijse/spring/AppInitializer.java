6d0e22327139ded9a3ac0cb5b51c63bf34ff2718
ChavinduShirantha <chavindushirantha@gmail.com>
2023-09-29 17:43:49 +0530

    Added 4th module

---
+++ O4_DI_In_Spring/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring;
+
+/**
+ * @author : Chavindu
+ * created : 9/28/2023-10:26 AM
+ **/
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+    }
+}
