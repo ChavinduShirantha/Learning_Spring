36303ae3c71f07dc7959c35d34cc1c90fee77fa8
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-01 15:42:13 +0530

    Updated Content

---
+++ O6_Spring_Configurations/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring;
+
+/**
+ * @author : Chavindu
+ * created : 10/1/2023-3:41 PM
+ **/
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+    }
+}
