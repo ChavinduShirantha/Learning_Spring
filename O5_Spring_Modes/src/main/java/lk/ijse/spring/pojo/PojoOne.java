9a89478fe6c4c3e409c3fd4b38f0f0cb82af5f3e
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-01 15:38:49 +0530

    Use Light Mode

--- O5_Spring_Modes/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ O5_Spring_Modes/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -1,9 +1,35 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
 /**
  * @author : Chavindu
  * created : 10/1/2023-3:29 PM
  **/
-
+@Component
 public class PojoOne {
+
+    public PojoOne(){
+        System.out.println("Pojo 1 Instantiate");
+    }
+
+    /*Light mode*/
+
+    @Bean
+    public PojoTwo pojo2() {
+//        Inter bean dependencies
+//        Inter bean dependency invocation
+        PojoThree pojo3 = pojo3();
+        PojoThree pojo03 = pojo3();
+        System.out.println(pojo3);
+        System.out.println(pojo03);
+
+        return new PojoTwo();
+    }
+
+    @Bean
+    public PojoThree pojo3() {
+        return new PojoThree();
+    }
 }
