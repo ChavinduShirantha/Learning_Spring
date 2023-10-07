d9de355653a44f021da2f9ff1aa5c37c090456b7
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-07 16:18:34 +0530

    Uses of OS and java System variables

---
+++ O8_Spring_Environment/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,43 @@
+package lk.ijse.spring;
+
+import lk.ijse.spring.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @author : Chavindu
+ * created : 10/7/2023-4:15 PM
+ **/
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+
+
+        System.out.println("=========================================================");
+
+        Map<String, String> getenv = System.getenv();
+
+        for (String key : getenv.keySet()) {
+            String value = getenv.get(key);
+            System.out.println(key + " : " + value);
+        }
+
+        System.out.println("=========================================================");
+
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            Object value = properties.get(key);
+            System.out.println(key + " : " + value);
+        }
+
+        System.out.println("=========================================================");
+
+
+    }
+}
---
+++ O8_Spring_Environment/src/main/java/lk/ijse/spring/pojo/Pojo.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.pojo;
+
+/**
+ * @author : Chavindu
+ * created : 10/7/2023-4:15 PM
+ **/
+
+public class Pojo {
+}
