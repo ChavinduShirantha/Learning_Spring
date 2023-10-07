d4c7113ee265e6c0cfdb8dbc1fad5f55f889c948
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-07 16:20:39 +0530

    Updated Content

--- O8_Spring_Environment/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ O8_Spring_Environment/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -1,9 +1,16 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author : Chavindu
  * created : 10/7/2023-4:15 PM
  **/
-
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
 public class AppConfig {
+    public AppConfig(){
+        System.out.println("AppConfig Instantiate");
+    }
 }
--- O8_Spring_Environment/src/main/java/lk/ijse/spring/pojo/Pojo.java
+++ O8_Spring_Environment/src/main/java/lk/ijse/spring/pojo/Pojo.java
@@ -1,9 +1,14 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author : Chavindu
  * created : 10/7/2023-4:15 PM
  **/
-
+@Component
 public class Pojo {
+    public Pojo() {
+        System.out.println("Pojo Instantiate");
+    }
 }
