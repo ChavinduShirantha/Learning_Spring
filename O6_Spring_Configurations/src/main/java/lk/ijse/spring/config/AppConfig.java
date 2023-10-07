e3654004e88836b1715b2b96ecafa8fc02228057
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-07 11:10:10 +0530

    Updated Content

--- O6_Spring_Configurations/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ O6_Spring_Configurations/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -1,10 +1,10 @@
 package lk.ijse.spring.config;
 
 import lk.ijse.spring.pojo.PojoOne;
-import lk.ijse.spring.pojo.PojoThree;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author : Chavindu
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring.pojo")
+@Import({ConfigTwo.class,ConfigThree.class})
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig Instantiate");
