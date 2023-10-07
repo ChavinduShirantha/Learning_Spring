2b494f911f00c9626b63f8e6f437ae2982d257a6
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-07 16:54:20 +0530

    Updated Content

--- O10_Controllers/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ O10_Controllers/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,9 +1,18 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @author : Chavindu
  * created : 10/7/2023-4:51 PM
  **/
-
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
 public class WebAppConfig {
+    public WebAppConfig(){
+        System.out.println("WebAppConfig Instantiated");
+    }
 }
--- O10_Controllers/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ O10_Controllers/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -1,9 +1,14 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author : Chavindu
  * created : 10/7/2023-4:51 PM
  **/
-
+@Configuration
 public class WebRootConfig {
+    public WebRootConfig(){
+        System.out.println("WebRootConfig : Instantiated");
+    }
 }
