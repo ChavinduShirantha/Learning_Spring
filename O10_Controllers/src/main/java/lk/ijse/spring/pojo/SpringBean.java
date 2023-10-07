84146f0d9463dae5f892602b5c384d68bf3d4d56
ChavinduShirantha <chavindushirantha@gmail.com>
2023-10-07 16:53:13 +0530

    Updated Content

---
+++ O10_Controllers/src/main/java/lk/ijse/spring/pojo/SpringBean.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Chavindu
+ * created : 10/7/2023-4:52 PM
+ **/
+
+@Component
+public class SpringBean {
+    public SpringBean(){
+        System.out.println("SpringBean Instantiated");
+    }
+}
